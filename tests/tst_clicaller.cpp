0d69b91e5b97901abd8fcff1c648ed6ef8338f3b
Denis Gofman <sendevent@gmail.com>
2020-05-20 13:40:06 +0300

    test for CLICaller

---
+++ tests/tst_clicaller.cpp
@@ -0,0 +1,52 @@
+/*
+   Copyright (C) 2020 Denis Gofman - <sendevent@gmail.com>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public License
+   along with this program. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.
+*/
+
+#include "tst_clicaller.h"
+
+#include "clicaller.h"
+#include "tst_action.h"
+
+#include <QElapsedTimer>
+#include <QSignalSpy>
+#include <QtTest>
+
+void tst_CLICaller::test_performAction()
+{
+    Action::Ptr action(new tst_Action(Action::ActScope::User, KnownAction::Unknown));
+    action->setApp("/usr/bin/ls");
+    action->setArgs({ "-la" });
+
+    bool actionPerformed(false);
+    connect(action.get(), &Action::performed, this,
+            [&actionPerformed](const QString & /*res*/, bool /*ok*/) { actionPerformed = true; });
+
+    QSignalSpy spy(action.get(), &Action::performed);
+
+    std::unique_ptr<CLICaller> caller(new CLICaller);
+    caller->performAction(action.get());
+
+    QElapsedTimer timer;
+    timer.start();
+    while (!actionPerformed && timer.elapsed() < CLICall::DefaultTimeoutMSecs)
+        QTest::qWait(10);
+
+    QCOMPARE(spy.count(), 1);
+    const QList<QVariant> &arguments = spy.takeFirst();
+    QVERIFY(arguments.at(0).type() == QVariant::String);
+    QVERIFY(arguments.at(1).type() == QVariant::Bool);
+    QVERIFY(arguments.at(1).toBool() == true);
+}
