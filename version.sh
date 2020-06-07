0d47afc4965644ef9af2e5dbe9938c0b5edcd98c
Denis Gofman <sendevent@gmail.com>
2020-06-07 04:05:55 +0300

    snap image build

--- version.sh
+++ version.sh
@@ -3,6 +3,10 @@
 CONF_MAJOR="1"
 CONF_MINOR="0"
 CONF_PATCH="0"
+CONF_BUILDTIME=`date +%s`
+CONF_BRANCH=""
+CONF_HASH=""
+CONF_DIRTY=""
 
 function assignVars()
 {
@@ -28,11 +32,23 @@ function setPatch() {
     assignVars $1 $CONF_PATCH
 }
 
+function setDirtyMark()
+{
+
+echo $dirtyMark
+    assignVars $1 $dirtyMark
+}
+
 function setRevision() {
-    local branch="`git rev-parse --abbrev-ref HEAD`"
-    local commit="`git rev-parse --short HEAD`"
-    local dirty=$([ ! -z "`git diff-index --name-only HEAD`" ] && echo "*" || echo "")
-    local info="[$commit@$branch$dirty]"
+    assignVars CONF_BRANCH $(git rev-parse --abbrev-ref HEAD)
+    assignVars CONF_HASH $(git rev-parse --short HEAD)
+
+    local dirtyFiles=$(git diff-index --name-only HEAD)
+    if [[ ! -z "$dirtyFiles" ]]; then
+        assignVars CONF_DIRTY "*"
+    fi
+
+    local info="[$CONF_HASH@$CONF_BRANCH$CONF_DIRTY]"
 
     assignVars $1 $info
 }
@@ -61,24 +77,23 @@ case $i in
     echo $REVISION
     exit 0
     ;;
+    e)
+    echo $CONF_BUILDTIME
+    exit 0
+    ;;
 esac
 done
 
-VERSION=""
-for j in $MAJOR $MINOR $PATCH $REVISION
-do
-    if [ ! -z "$j" ]
-    then
-        if [ ! -z "$VERSION" ]
-        then
-              VERSION="$VERSION."
-        fi
+VERSION=$(printf '%s.%s.%s %s' "$MAJOR" "$MINOR" "$PATCH" "$REVISION") # 1.0.0 [xyz@master*]
+VERSION_SNAP=$(printf '%s.%s.%s-%s-%s%s' "$MAJOR" "$MINOR" "$PATCH" "$CONF_BRANCH" "$CONF_HASH" "$CONF_DIRTY") # 1.0.0-master-xyz*]
+VERSION_SNAP="${VERSION_SNAP/\*/\+}" # 1.0.0.[xyz@master*] -> 1.0.0-master-xyz*] -> # 1.0.0-master-xyz+]
 
-        VERSION="$VERSION$j"
-    fi
-done
+# for dev local branch:
+VERSION_SNAP="${VERSION_SNAP/_/-}" # 1.0.0.[xyz@dengof_snap] -> 1.0.0.[xyz@dengof-snap]
+VERSION_SNAP="${VERSION_SNAP/\#/N}" # 1.0.0.[xyz@issue_#123] -> 1.0.0.[xyz@issue-N123]
 
-# 1.0.0.[xyz@master*] -> 1.0.0 [xyz@master*]
-VERSION="${VERSION/.[/ [}"
+metaFile=yangl.metainfo.xml
+#sed -E -e "0,/(<release.*\/>)/ s//\1\n<release date=\"`date -d @$CONF_BUILDTIME +%Y-%m-%d`\" version=\"$VERSION_SNAP\" \/>/" ./$metaFile.in > ./$metaFile
+sed -E -e "0,/(replaceme)/ s//\n<release date=\"`date -d @$CONF_BUILDTIME +%Y-%m-%d`\" version=\"$VERSION_SNAP\" \/>/" ./$metaFile.in > ./$metaFile
 
 echo $VERSION
