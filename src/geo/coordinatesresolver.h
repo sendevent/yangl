40f9f4722835f4af43be7682583bf58e28c65b38
Denis Gofman <sendevent@gmail.com>
2025-04-25 14:18:32 +0200

    Add offline coordinates detection basics and tests

--- src/geo/coordinatesresolver.h
+++ src/geo/coordinatesresolver.h
@@ -2,10 +2,30 @@
 
 #include <QGeoCoordinate>
 #include <QObject>
-#include <qgeocoordinate.h>
+#include <qhash.h>
 
-using CityInfo = QPair<QString, QGeoCoordinate>;
-using CountryInfo = QSet<CityInfo>;
+struct PlaceInfo {
+    QString country;
+    QString region;
+    QString town;
+    QGeoCoordinate location;
+    bool ok { false };
+    QString message;
+};
+
+inline bool operator==(const PlaceInfo &lhs, const PlaceInfo &rhs)
+{
+    return lhs.country == rhs.country && lhs.region == rhs.region && lhs.town == rhs.town
+            && lhs.location == rhs.location /*&& lhs.ok == rhs.ok && lhs.message == rhs.message*/;
+}
+
+inline uint qHash(const PlaceInfo &key, uint seed = 0)
+{
+    return qHashMulti(seed, key.country, key.region, key.town, key.location.latitude(), key.location.longitude()/*,
+                      key.ok, key.message*/);
+}
+
+using CountryInfo = QHash<QString, QMultiHash<QString, PlaceInfo>>;
 using CitiesByCountry = QHash<QString, CountryInfo>;
 
 class CoordinatesResolver : public QObject
@@ -14,17 +34,9 @@ class CoordinatesResolver : public QObject
 public:
     explicit CoordinatesResolver(QObject *parent = nullptr);
 
-    struct PlaceInfo {
-        QString country;
-        QString region;
-        QString town;
-        QGeoCoordinate location;
-        bool ok { false };
-        QString message;
-    };
-
 public:
     void requestCoordinates(const PlaceInfo &town);
+    void requestCoordinates(const QString &country, const QString &city);
 
 signals:
     void coordinatesResoloved(const PlaceInfo &town);
@@ -35,11 +47,14 @@ private:
     CitiesByCountry m_data;
 
     void ensureDataLoaded();
-    void loadDataBuiltin();
-    void loadDataDynamic();
-    void loadData(const QString &path);
+    bool loadDataBuiltin();
+    bool loadDataDynamic();
+    CitiesByCountry loadData(const QString &path);
 
-    void lookupForPlace(const PlaceInfo &town);
+    PlaceInfo lookupForPlace(const PlaceInfo &request) const;
+    PlaceInfo lookupForPlace(const QString &country, const QString &city) const;
 
 private slots:
+
+    friend class TestCoordinatesResolver;
 };
