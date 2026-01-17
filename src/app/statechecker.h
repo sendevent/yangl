8f83469dcedfc13884fdb2e02f0a575e3077ff3a
Denis Gofman <sendevent@gmail.com>
2026-01-17 11:14:04 +0100

    Reduce polling interval to 1s on any status change for 30s

--- src/app/statechecker.h
+++ src/app/statechecker.h
@@ -29,7 +29,18 @@ class StateChecker : public QObject
 {
     Q_OBJECT
 public:
+    enum class PollingMode
+    {
+        Dynamic,
+        Custom,
+    };
+    Q_ENUM(PollingMode)
+
     static const int DefaultIntervalMs;
+    static const int MaxConsecutiveErrors;
+    static const int DynamicIntervalTransitionalMs;
+    static const int DynamicIntervalStableMs;
+    static const int DynamicTransitionTimeoutMs;
 
     using Ptr = QSharedPointer<StateChecker>;
     explicit StateChecker(CLICaller *bus, int intervalMs);
@@ -45,6 +56,7 @@ public:
 public slots:
     void setInterval(int msecs);
     void setActive(bool active);
+    void setPollingMode(PollingMode mode);
 
 signals:
     void stateChanged(const NordVpnInfo &state);
@@ -54,11 +66,18 @@ signals:
 private slots:
     void onTimeout();
     void onQueryFinish(const Action::Id &id, const QString &result, bool ok, const Action::RunInfo &info);
+    void onUptimeTick();
 
 protected:
     CLICaller *m_bus;
     Action::Ptr m_actCheck;
     QTimer *m_timer;
+    bool m_pollInFlight;
+    int m_consecutiveErrors;
+    QTimer *m_uptimeTicker;
+    QTimer *m_transitionTimer;
+    PollingMode m_pollingMode;
+    int m_customIntervalMs;
 
     NordVpnInfo m_state;
     void setState(const NordVpnInfo &state);
@@ -69,6 +88,8 @@ protected:
     void notifyError(const QString &errorMessage);
 
     void stopTimer();
+    void endTransition();
+    void adjustDynamicInterval();
 
     friend class TestStateChecker;
     friend class NordVpnWrapper;
