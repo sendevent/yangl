e7d35de20c1f5ec588a6c03973a2a1bc04200a54
Denis Gofman <sendevent@gmail.com>
2026-03-06 20:57:14 +0100

    Add NordVpnInfo::fromString tests

---
+++ test/tests/app/nordvpninfo/testnordvpninfo.cpp
@@ -0,0 +1,79 @@
+/*
+   Copyright (C) 2020-2026 Denis Gofman - <sendevent@gmail.com>
+
+   This application is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 3 of the License, or (at your option) any later version.
+
+   This application is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public License
+   along with this program. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.
+*/
+
+#include "app/nordvpninfo.h"
+
+#include <QObject>
+#include <QTest>
+
+class TestNordVpnInfo : public QObject
+{
+    Q_OBJECT
+
+private slots:
+    void test_fromString_emptyInput();
+    void test_fromString_malformedLine();
+    void test_fromString_missingStatus();
+    void test_fromString_invalidStatus();
+    void test_fromString_invalidUptime();
+    void test_fromString_valid();
+};
+
+void TestNordVpnInfo::test_fromString_emptyInput()
+{
+    const NordVpnInfo parsed = NordVpnInfo::fromString({});
+    QCOMPARE(parsed, NordVpnInfo {});
+}
+
+void TestNordVpnInfo::test_fromString_malformedLine()
+{
+    const NordVpnInfo parsed = NordVpnInfo::fromString("Status Connected\nServer: srv");
+    QCOMPARE(parsed, NordVpnInfo {});
+}
+
+void TestNordVpnInfo::test_fromString_missingStatus()
+{
+    const NordVpnInfo parsed = NordVpnInfo::fromString("Server: srv\nCountry: Country");
+    QCOMPARE(parsed, NordVpnInfo {});
+}
+
+void TestNordVpnInfo::test_fromString_invalidStatus()
+{
+    const NordVpnInfo parsed = NordVpnInfo::fromString("Status: TotallyInvalid\nServer: srv");
+    QCOMPARE(parsed, NordVpnInfo {});
+}
+
+void TestNordVpnInfo::test_fromString_invalidUptime()
+{
+    const QString input = "Status: Connected\nServer: srv\nUptime: nope tokens";
+    const NordVpnInfo parsed = NordVpnInfo::fromString(input);
+    QCOMPARE(parsed, NordVpnInfo {});
+}
+
+void TestNordVpnInfo::test_fromString_valid()
+{
+    const QString input = "Status: Connected\nServer: test.server\nCountry: Neverland\nCity: TestCity\nUptime: 1 day 2 hours";
+    const NordVpnInfo parsed = NordVpnInfo::fromString(input);
+
+    QCOMPARE(parsed.status(), NordVpnInfo::Status::Connected);
+    QCOMPARE(parsed.server(), QString("test.server"));
+    QCOMPARE(parsed.country(), QString("Neverland"));
+    QCOMPARE(parsed.city(), QString("TestCity"));
+}
+
+QTEST_MAIN(TestNordVpnInfo)
+#include "testnordvpninfo.moc"
